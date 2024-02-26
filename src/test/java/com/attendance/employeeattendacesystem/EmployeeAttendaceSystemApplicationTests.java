c2c830330e4756ff438aeae7b98e9a7c7a3a1924
chuman <chuman.panda@gmail.com>
2024-02-26 19:00:04 +0530

    first commit

---
+++ src/test/java/com/attendance/employeeattendacesystem/EmployeeAttendaceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendance.employeeattendacesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAttendaceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
