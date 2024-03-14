acad5d285edddc341f326fc13565397641467cac
manumang29 <manumang29@gmail.com>
2024-03-14 15:33:20 +0530

    first commit

---
+++ Sring-security/src/main/java/com/javatechie/sringsecurity/SringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.sringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SringSecurityApplication.class, args);
+    }
+
+}
